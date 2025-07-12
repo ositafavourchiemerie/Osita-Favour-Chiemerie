df42ae2e1329f546e2a7823b78564092a51951a0
ositafavourchiemerie <Ositafavour87@gmail.com>
2025-07-12 10:36:37 +0100

    Create Cos.py

---
+++ Cos.py
@@ -0,0 +1,15 @@
+def get_grade(score):
+    if 70 <= score <= 100:
+        return "A"
+    elif 60 <= score <= 69:
+        return "B"
+    elif 50 <= score <= 59:
+        return "C"
+    elif 45 <= score <= 49:
+        return "D"
+    elif 40 <= score <= 44:
+        return "E"
+    elif 0 <= score < 40:
+        return "F"
+    else:
+        return "Invalid score"
